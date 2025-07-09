b92fd24be2f63557ec5053ef64749702cd192adb
Gabriel Maldonado <gabinho767@gmail.com>
2025-07-08 21:04:49 -0400

    Remove API key middleware to test with RapidAPI

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ValidationController;
 use App\Http\Controllers\AdvancedValidationController;
 
-Route::middleware(App\Http\Middleware\ApiKeyMiddleware::class)->group(function () {
-    Route::post('/validate/phone', [ValidationController::class, 'validatePhone']);
-    Route::post('/validate/email', [AdvancedValidationController::class, 'validateEmail']);
-});
+
+Route::post('/validate/phone', [ValidationController::class, 'validatePhone']);
+Route::post('/validate/email', [AdvancedValidationController::class, 'validateEmail']);
+
 
 
